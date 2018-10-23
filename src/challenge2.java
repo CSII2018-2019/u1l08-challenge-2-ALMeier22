fb395f2b85845707a8cced569946c644ace0e454
antonia <antonia@antonias-air.lan>
2018-10-23 18:57:14 -0400

    test

---
+++ src/challenge2.java
@@ -0,0 +1,16 @@
+
+public class challenge2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int [] array1 = {1,3,2,4,6,7,5,8,10};
+		int sum = 0; 	
+		for(int i=0; i< array1.length; i++) {
+			sum = sum + array1[i];
+		}
+		int missing = 55 - sum;
+		System.out.print("The missing number is " + missing);
+	}
+
+}
